14f98ef100eb00a940d18834738c7cdfa8c8117e
Cole Vertikoff <cole.vertikoff@gmail.com>
2018-04-04 12:04:39 -0400

    Adds autocomplete to selector. Currently, the values are hardcoded

--- hr-frontend/src/TextInput.js
+++ hr-frontend/src/TextInput.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import TextField from '../node_modules/material-ui/TextField';
 import Button from '../node_modules/material-ui/RaisedButton';
+import AutoComplete from '../node_modules/material-ui/AutoComplete';
 
 
 class TextInput extends React.Component {
@@ -9,15 +10,19 @@ class TextInput extends React.Component {
 		super();
 		this.state = {
 			"nameTextField": "", // This is where the content for the TextField used below is stored
-      "data": ""
+			"dataSource": [
+        "suyash@suyashkumar.com",
+        "cole@cole.com"
+      ],
+			"data": ""
 		}
 	}
 
 
 
-	onNameTextFieldChange = (event) => {
+	handleInput = (value) => {
 		// Update the nameTextField state whenever the text field is changed or perturbed in any way:
-		this.setState({"nameTextField": event.target.value});
+		this.setState({"nameTextField": value});
 	}
 
 	getData = (event) => {
@@ -32,13 +37,11 @@ class TextInput extends React.Component {
 	render() {
 		return (
 			<div class="text-input-class-holders">
-				<TextField
-					value={this.state.nameTextField}
-					onChange={this.onNameTextFieldChange}/>
+				<AutoComplete hintText="Email address"
+					dataSource={this.state.dataSource}
+					onUpdateInput={this.handleInput}/>
         <section>
-				<Button onClick={this.getData}>
-					Get HR Data
-				</Button>
+				<Button onClick={this.getData} primary={true} label="Get heart rate data" />
         </section>
 				{this.state.data /*show the current nameTextField state here in the browser */}
 			</div>
